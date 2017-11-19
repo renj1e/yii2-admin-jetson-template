74ede784547ec57eaa7718d535df0538d59baa24
renjelc <renjelc@gmail.com>
2017-11-19 21:10:44 +0800

    File structure update

---
+++ helpers/AppAsset.php
@@ -0,0 +1,20 @@
+<?php
+namespace backend\assets\kosmo;
+
+use yii\base\Exception;
+use yii\web\AssetBundle as BaseAppAsset;
+
+/**
+ * AdminLte AssetBundle
+ * @since 0.1
+ */
+class AppAsset extends BaseAppAsset
+{
+    public $sourcePath = '@vendor/renj1e/yii2-kosmo-template/src/';
+    public $css = [
+    ];
+    public $js = [
+    ];
+    public $depends = [
+    ];
+}
