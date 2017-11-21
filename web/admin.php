c6b8aabfc4bd7a4783e733f07ddb9ea133317f5e
renjelc <renjelc@gmail.com>
2017-11-21 09:53:22 +0800

    style.css update

--- web/admin.php
+++ web/admin.php
@@ -20,22 +20,25 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/renj1e/yii2-
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <link type="text/css" href="http://chat.agency.dev/cometchat/cometchatcss.php" rel="stylesheet" charset="utf-8">
-<script type="text/javascript" src="http://chat.agency.dev/cometchat/cometchatjs.php" charset="utf-8"></script>
     <?php $this->head() ?>
 </head>
-<body class="ks-navbar-fixed ks-sidebar-default ks-sidebar-fixed ks-theme-jungle-green ks-page-loading">
+<body>
 <?php $this->beginBody() ?>
+    <!-- Preloader -->
+    <div class="preloader-it">
+        <div class="la-anim-1"></div>
+    </div>
+    <!-- /Preloader -->
     <!-- BEGIN HEADER -->
-    <nav class="navbar ks-navbar">
+
 
         <?= $this->render(
             'partials/header.php',
             ['directoryAsset' => $directoryAsset]
         ) ?>
-    </nav>
 
-    <div class="ks-container">
+
+
         <?= $this->render(
             'partials/left.php',
             ['directoryAsset' => $directoryAsset]
@@ -46,9 +49,7 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/renj1e/yii2-
             'partials/content.php',
             ['content' => $content, 'directoryAsset' => $directoryAsset]
         ) ?>
-
-    </div>
-</div>
+        
 
 <?php $this->endBody() ?>
 </body>
